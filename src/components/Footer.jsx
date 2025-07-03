7d48ae5d78c23ccf50a192ff4f3a58f446876188
jeronimosardina <jeronimosardina@gmail.com>
2025-07-03 11:06:57 -0300

    Fix: add Footer or remove import

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer style={{ textAlign: "center", padding: "1rem", color: "#999" }}>
+        <p>creado por Jeronimo Sardina</p>
+      </footer>
+    );
+  }
+  
